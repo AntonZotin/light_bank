1c7486eac74563b7027b87b9dad6e7f0e05b6d4c
AntonZotin <toxin88@mail.ru>
2024-05-07 11:25:11 +0300

    refactor

--- src/test/java/com/bank/light/config/TestRabbitConfiguration.java
+++ src/test/java/com/bank/light/config/TestRabbitConfiguration.java
@@ -3,10 +3,8 @@ package com.bank.light.config;
 import com.github.fridujo.rabbitmq.mock.compatibility.MockConnectionFactoryFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class TestRabbitConfiguration {
