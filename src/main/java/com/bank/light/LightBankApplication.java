295fef85b9603c2036fd8b51f504d88443cf6fd0
AntonZotin <toxin88@mail.ru>
2024-05-07 09:55:44 +0300

    refactor

--- src/main/java/com/bank/light/LightBankApplication.java
+++ src/main/java/com/bank/light/LightBankApplication.java
@@ -2,10 +2,8 @@ package com.bank.light;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement
 public class LightBankApplication {
     public static void main(String[] args) {
         SpringApplication.run(LightBankApplication.class, args);
